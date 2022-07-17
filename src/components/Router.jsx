11444fc92a798076db9168f42a4ab431fe68dc6d
pongdang <pongdang0121@gmail.com>
2022-07-18 08:51:58 +0900

    feat: 라우터 설정

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
 import Profile from "../pages/Profile";
 
 export default function Router() {
   return (
     <Routes>
+      <Route path='/' element={<Home />} />
       <Route path='/profile' element={<Profile />} />
     </Routes>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <section>
+      <h1>Home</h1>
+    </section>
+  );
+}
