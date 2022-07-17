8e2fe6ddc2e4b481f0783e99386533e3c5f5fd98
pongdang <pongdang0121@gmail.com>
2022-07-17 20:54:47 +0900

    feat: 라우터 설정

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./components/Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      <Router />
+    </BrowserRouter>
   </React.StrictMode>
 );
