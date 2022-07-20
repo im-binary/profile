0c0501bc84673724c4330df2b68a7e4ddcb21b95
pongdang <pongdang0121@gmail.com>
2022-07-20 22:24:53 +0900

    chore: url 변수로 관리

---
+++ src/constant.js
@@ -0,0 +1,9 @@
+export const URLs = {
+  email: "pongpong0121@kakao.com",
+  github: "https://github.com/pongdang",
+  velog: "https://velog.io/@pongdang",
+  pongdangToday: "https://github.com/pongdang/pongdang.github.io",
+  pongdangWorld: "https://github.com/pongdang/pongdang.world",
+  pongdangDiary: "https://github.com/pongdang/pongdang-diary",
+  searchJJAL: "https://github.com/pongdang/cat-project",
+};
