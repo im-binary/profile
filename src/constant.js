c2a96641230ef9cbb7ee7bc577f1ff5f5cb492bf
pongdang <pongdang0121@gmail.com>
2022-07-20 23:56:47 +0900

    feat: 잘못된 링크 수정

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
 export const URLs = {
-  email: "pongpong0121@kakao.com",
+  email: "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=pongpong0121@kakao.com",
   github: "https://github.com/pongdang",
   velog: "https://velog.io/@pongdang",
   pongdangToday: "https://github.com/pongdang/pongdang.github.io",
