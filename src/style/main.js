9fcfba141c7e3735a94ee8c9f667e4c1e97cd6d0
pongdang <pongdang0121@gmail.com>
2022-07-21 00:50:24 +0900

    chore: 스타일 변수 지정

--- src/style/main.js
+++ src/style/main.js
@@ -1,9 +1,26 @@
 import { css } from "@emotion/react";
 
 export const mainSectionContainer = css`
-  width: 100%;
-  border: 2px solid;
-  border-radius: 20px;
-  height: 100%;
-  padding: 20px;
+  border: 1px solid;
+  min-height: calc(100vh - 43px);
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 100px 40px;
+`;
+
+export const garyTitleBorderBottom = css`
+  border-bottom: 1px solid rgba(55, 53, 47, 0.16);
+  padding-bottom: 8px;
+`;
+
+export const garyNomalBorderBottom = css`
+  border-bottom: 1px solid rgba(55, 53, 47, 0.16);
+`;
+
+export const grayBorder = css`
+  border: 1px solid rgba(55, 53, 47, 0.16);
+`;
+
+export const contentFontSize = css`
+  font-size: 1.6rem;
 `;
