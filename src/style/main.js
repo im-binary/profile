a751b7e8af31f5873790a24015ec442902f6b0e0
pongdang <pongdang0121@gmail.com>
2022-07-21 03:02:48 +0900

    chore:  자주 쓰는 스타일 변수로 관리

--- src/style/main.js
+++ src/style/main.js
@@ -21,6 +21,20 @@ export const grayBorder = css`
   border: 1px solid rgba(55, 53, 47, 0.16);
 `;
 
-export const contentFontSize = css`
+export const contentFontSize16 = css`
   font-size: 1.6rem;
 `;
+
+export const contentFontSize30 = css`
+  font-size: 3rem;
+`;
+
+export const contentFontSize40 = css`
+  font-size: 4rem;
+`;
+
+export const mainColor = "rgba(135,131,120,0.15)";
+
+export const subColor = "rgb(208,199,173,0.16)";
+
+export const boxTextColor = "#EB5757";
