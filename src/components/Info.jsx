dc63e3cabd0e887f09d905238001382fc0e5c36f
pongdang <pongdang0121@gmail.com>
2022-07-20 23:56:29 +0900

    feat: info 컴포넌트 구조

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -6,32 +6,94 @@ import { URLs } from "../constant";
 
 export default function Info() {
   return (
-    <section>
-      <img src='https://avatars.githubusercontent.com/u/76990149?v=4' alt='프로필 사진(고양이)' />
-      <h1>전이진 | Profile</h1>
-      <p>My status is ... 🐱</p>
-      <p>
-        <span>🙌</span> 안녕하세요! 프론트엔드 개발자를 지망하고 있는 전이진입니다.👻
-      </p>
-      <article>
+    <article css={infoContainer}>
+      <img src='https://avatars.githubusercontent.com/u/76990149?v=4' alt='프로필 고양이 사진' />
+      <h1>Profile | 전이진</h1>
+      <section>
+        <p className='description'>
+          <span>🙌</span>
+          <span>
+            프론트엔드 개발자를 지망하고 있는 전이진입니다.
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta modi quas obcaecati nisi maxime facilis et.
+          </span>
+        </p>
         <ul>
           <li>
             <a href={URLs.email} target='_blank' rel='noreferrer'>
-              pongpong0121@kakao.com
+              ✉️ <span>pongpong0121@kakao.com</span>
             </a>
           </li>
           <li>
             <a href={URLs.github} target='_blank' rel='noreferrer'>
-              Github
+              👩‍💻 <span>Github</span>
             </a>
           </li>
           <li>
             <a href={URLs.velog} target='_blank' rel='noreferrer'>
-              Velog
+              ✏️ <span>Velog</span>
             </a>
           </li>
         </ul>
-      </article>
-    </section>
+      </section>
+    </article>
   );
 }
+
+const infoContainer = css`
+  img {
+    width: 256px;
+    height: 256px;
+    border-radius: 50%;
+  }
+
+  h1 {
+    margin: 40px 0;
+    font-size: 4rem;
+  }
+
+  p {
+    border: 1px solid rgba(55, 53, 47, 0.16);
+    border-radius: 20px;
+    padding: 20px;
+  }
+
+  section {
+    display: flex;
+    justify-content: space-between;
+    gap: 30px;
+    line-height: 1.5;
+    font-size: 1.6rem;
+  }
+
+  .description {
+    width: 60%;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    span:first-of-type {
+      height: 100%;
+      margin-top: 14px;
+    }
+  }
+
+  ul {
+    width: 40%;
+    padding: 20px;
+    overflow: scroll;
+
+    li {
+      margin: 4px 0;
+
+      a span {
+        margin-right: 10px;
+        vertical-align: middle;
+        /* text-decoration-line: underline; */
+        border-bottom: 0.1rem solid rgba(55, 53, 47, 0.16);
+      }
+    }
+  }
+
+  // 817px 이하일 때 미디어 쿼리 작성
+`;
