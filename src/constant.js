f4f295d17d3482766d451cb041bc479e715d0899
pongdang <pongdang0121@gmail.com>
2022-07-27 14:09:41 +0900

    chore: 안 쓰는 url 삭제

--- src/constant.js
+++ src/constant.js
@@ -2,9 +2,4 @@ export const URLs = {
   email: "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=pongpong0121@kakao.com",
   github: "https://github.com/pongdang",
   velog: "https://velog.io/@pongdang",
-  pongdangToday: "https://github.com/pongdang/pongdang.github.io",
-  pongdangWorld: "https://github.com/pongdang/pongdang.world",
-  pongdangDiary: "https://github.com/pongdang/pongdang-diary",
-  searchJJAL: "https://github.com/pongdang/cat-project",
-  functionalCodingBook: "https://velog.io/@pongdang/series/쏙쏙-들어오는-함수형-코딩",
 };
