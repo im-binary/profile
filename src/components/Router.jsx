6ec293d4efbdee276b40c8081f43f8fa7276a114
pongdang <pongdang0121@gmail.com>
2022-07-29 11:47:49 +0900

    feat: 페이지 이동할 때마다 맨 위로 scroll

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Profile from "../pages/Profile";
+import { useLocation } from "react-router-dom";
 
 export default function Router() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    console.log("Router");
+  }, [pathname]);
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
