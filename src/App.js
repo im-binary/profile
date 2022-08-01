5eb956ed940c139fc190045b6cf1b09f124ccafe
pongdang <pongdang0121@gmail.com>
2022-08-01 22:28:22 +0900

    feat: state 관리 컴포넌트 옮김

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import Header from "./components/Header";
+import ThemeButton from "./components/ThemeButton";
+import { useDispatch, useSelector } from "react-redux";
+import { changeThemeMode } from "./redux/actions/themeAction";
+import { useState } from "react";
 
 export default function App() {
-  return <Header />;
+  const state = useSelector((state) => state);
+  const [theme, setTheme] = useState(state);
+
+  const dispatch = useDispatch();
+
+  const handleChangeMode = () => {
+    dispatch(changeThemeMode(!theme));
+    setTheme(!theme);
+  };
+  return (
+    <>
+      <Header />
+      <ThemeButton handleChangeMode={handleChangeMode} />
+    </>
+  );
 }
