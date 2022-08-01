6038c9e2f6a494ff1efbbf7407ce26857c240ed8
pongdang <pongdang0121@gmail.com>
2022-08-01 20:41:18 +0900

    feat: global 스타일 지정

---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Global, css } from "@emotion/react";
+import { useSelector } from "react-redux";
+
+export default function GlobalStyle() {
+  const state = useSelector((state) => state);
+
+  return <Global styles={style(state)} />;
+}
+
+const style = (state) => css`
+  body {
+    background-color: ${state ? "#fff" : "#202124"};
+  }
+`;
