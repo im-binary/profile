f706edb739ce0a195c5851a33d884e9d776fde6c
pongdang <pongdang0121@gmail.com>
2022-08-01 20:43:22 +0900

    feat: provider 설정

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./components/Router";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import GlobalStyle from "./style/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <Router />
-    </BrowserRouter>
+    <Provider store={store}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <App />
+        <Router />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
