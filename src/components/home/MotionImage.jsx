ffe604634e67451efa1db0db0f8f04e1e010b547
pongdang <pongdang0121@gmail.com>
2022-08-01 20:44:46 +0900

    chore: 코드 여백 추가

--- src/components/home/MotionImage.jsx
+++ src/components/home/MotionImage.jsx
@@ -4,6 +4,7 @@ import { motion, useScroll, useSpring } from "framer-motion";
 
 export default function MotionImage() {
   const { scrollYProgress } = useScroll();
+
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
     damping: 30,
