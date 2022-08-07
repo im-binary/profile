ce3ae2cafca130416bf7afd17e27e66e7007e7f7
pongdang <pongdang0121@gmail.com>
2022-08-07 20:30:06 +0900

    feat: props 제거

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,19 +8,16 @@ import TechStack from "../components/Profile/TechStack";
 import Education from "../components/Profile/Education";
 import ProjectInfo from "../components/Profile/ProjectInfo";
 import BooksInfo from "../components/Profile/BooksInfo";
-import { useSelector } from "react-redux";
 
 export default function Profile() {
-  const state = useSelector((state) => state);
-
   return (
     <main css={profileContainer}>
-      <Info state={state} />
+      <Info />
       <Career />
       <TechStack />
       <Education />
-      <ProjectInfo state={state} />
-      <BooksInfo state={state} />
+      <ProjectInfo />
+      <BooksInfo />
     </main>
   );
 }
