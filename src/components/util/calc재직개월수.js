c2163d90844e4853d89d0a6a3d0e266bf2ea16f7
pongdang <pongpong0121@kakao.com>
2023-04-28 01:08:42 +0900

    feat: 재직 기간을 계산하는 함수 작성

---
+++ src/components/util/calc재직개월수.js
@@ -0,0 +1,18 @@
+export const calc재직개월수 = (startDate, stopDate = new Date()) => {
+  const startDateObj = new Date(startDate);
+  const stopDateObj = new Date(stopDate);
+
+  const 재직개월수 =
+    (stopDateObj.getFullYear() - startDateObj.getFullYear()) * 12 + (stopDateObj.getMonth() - startDateObj.getMonth());
+
+  if (재직개월수 >= 12) {
+    const years = Math.floor(재직개월수 / 12);
+    const month = 재직개월수 % 12;
+
+    if (month === 0) return `${years}년`;
+
+    return `${years}년 ${month}개월`;
+  }
+
+  return `${재직개월수}개월`;
+};
