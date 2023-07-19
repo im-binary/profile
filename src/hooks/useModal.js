8516fed27db2a127b573822d73e34eddf5ab7adf
pongdang <pongpong0121@kakao.com>
2023-07-19 18:41:14 +0900

    feat: useModal 함수 작성

---
+++ src/hooks/useModal.js
@@ -0,0 +1,27 @@
+import { useOverlay } from "@toss/use-overlay";
+import { useMemo } from "react";
+import Modal from "../components/common/Modal/Modal";
+
+export const useModal = () => {
+  const overlay = useOverlay();
+
+  return useMemo(
+    () => ({
+      open: ({ title, description, content = <></> }) =>
+        overlay.open(({ isOpen, close }) => {
+          return (
+            <Modal open={isOpen} onOpenChange={close}>
+              <Modal.Content>
+                {title && <Modal.Title>{title}</Modal.Title>}
+                {description && <Modal.Description></Modal.Description>}
+                {content}
+                <Modal.Close>𝗫</Modal.Close>
+              </Modal.Content>
+            </Modal>
+          );
+        }),
+      close: overlay.close,
+    }),
+    [overlay]
+  );
+};
